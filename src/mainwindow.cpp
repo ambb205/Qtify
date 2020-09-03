c868b8ac3b97dbd08b92c1a6b51d9b7ccbf2ec7f
Allen Baird <ambb205@gmail.com>
2020-09-03 15:56:52 -0700

    Added keys.json file for others to add their own keys when using app

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "include/mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFile>
-#include <QTextStream>
-#include <QPixmap>
+#include <QMessageBox>
+#include <QOAuthHttpServerReplyHandler>
 #include <QPalette>
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+#include <QPixmap>
+#include <QTextStream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+MainWindow::MainWindow(QString client_id, QString client_secret, QWidget *parent) :
+    QMainWindow(parent), ui(new Ui::MainWindow), client_id_(client_id), client_secret_(client_secret)
 {
     ui->setupUi(this);
     QFile f(":qdarkstyle/style.qss");
@@ -19,11 +22,24 @@ MainWindow::MainWindow(QWidget *parent)
         f.open(QFile::ReadOnly | QFile::Text);
         QTextStream ts(&f);
         qApp->setStyleSheet(ts.readAll());
+        f.close();
     }
+
+    //    auto replyHandler = new QOAuthHttpServerReplyHandler(8080, this);
+
+    //    spotify_.setReplyHandler(replyHandler);
+    //    spotify_.setAuthorizationUrl(QUrl("https://accounts.spotify.com/authorize"));
+    //    spotify_.setAccessTokenUrl(QUrl("https://accounts.spotify.com/api/token"));
+    //    spotify_.setClientIdentifier(client_id_);
+    //    spotify_.setClientIdentifierSharedKey(client_secret_);
+    //    spotify_.setScope("user-read-private "
+    //                      "user-top-read "
+    //                      "playlist-read-private "
+    //                      "playlist-modify-public "
+    //                      "playlist-modify-private");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
