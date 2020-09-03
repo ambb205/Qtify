a78f9f300ffabd787c7d2832fdf809c8195b9367
Allen Baird <ambb205@gmail.com>
2020-09-03 14:55:09 -0700

    Added base qt files/resources

---
+++ src/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "include/mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QPixmap>
+#include <QPalette>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QFile f(":qdarkstyle/style.qss");
+    if (!f.exists())
+    {
+        printf("Unable to set stylesheet, file not found\n");
+    }
+    else
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        qApp->setStyleSheet(ts.readAll());
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
